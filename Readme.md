da6e9004fe294ef520bb5370e36c5e39934beec2
Shekhar Tyagi <notshekhar@gmail.com>
2020-12-12 21:26:46 +0530

    new

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Features to Implement
+
+-   [ ] Add Mute and Unmute option
+-   [ ] Add Video camera on/off button
+-   [ ] Switch camera device and audio devices
+-   [ ] Screen Share
+-   [ ] Process Audio and video before sending it to another user in the room using neuralnet
+-   [ ] Auth user before creating and joining the meeting so that can keep track of name and all
+-   [ ] Meeting recording button
+-   [ ] 
--- public/room.js
+++ public/room.js
@@ -1,94 +1,117 @@
-let video_count = 0
-
-let socket = io("/")
-// const user_id = v4()
-let calls = {}
-
-//production
-const peer = new Peer(undefined, {
-    host: "paperpeerserver.herokuapp.com",
-    secure: true,
-    port: 443,
-})
-
-//development
-// const peer = new Peer(undefined, {
-//     host: "/",
-//     port: 3001,
-// })
-peer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id)
-})
-peer.on("connection", (connection) => {
-    console.log(connection)
-})
-socket.on("user-disconnected", (uid) => {
-    console.log("disconnected", uid)
-    if (calls[uid]) calls[uid].close()
-})
-
-function hasMediaDevises() {
-    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+function showLoading(message) {
+    let loading = document.querySelector(".loading")
+    loading.style.display = "flex"
+    loading.innerHTML = message
 }
+function hideLoading() {
+    let loading = document.querySelector(".loading")
+    loading.style.display = "none"
+    loading.innerHTML = ""
+}
+async function wakupPeerServerHeruko() {
+    showLoading("Loading ...")
+    fetch("https://paperpeerserver.herokuapp.com").then((res) => {
+        if (res.status == 200) {
+            hideLoading()
+            Init()
+        } else {
+        }
+    })
+}
+wakupPeerServerHeruko()
 
-if (hasMediaDevises()) {
-    navigator.mediaDevices
-        .getUserMedia({
-            video: true,
-            audio: true,
-        })
-        .then((stream) => {
-            window.stream = stream
-            const myVideo = document.createElement("video")
-            myVideo.muted = true
-            addVideo(myVideo, stream)
-            peer.on("call", (call) => {
-                console.log("recevied a call")
-                call.answer(stream)
-                let video = document.createElement("video")
-                call.on("stream", (userVideoStream) => {
-                    addVideo(video, userVideoStream)
-                    recalculateLayout()
+function Init() {
+    let video_count = 0
+
+    let socket = io("/")
+    // const user_id = v4()
+    let calls = {}
+
+    //production
+    const peer = new Peer(undefined, {
+        host: "paperpeerserver.herokuapp.com",
+        secure: true,
+        port: 443,
+    })
+    //development
+    // const peer = new Peer(undefined, {
+    //     host: "/",
+    //     port: 3001,
+    // })
+    peer.on("open", (id) => {
+        socket.emit("join-room", ROOM_ID, id)
+    })
+    peer.on("connection", (connection) => {
+        console.log(connection)
+    })
+    socket.on("user-disconnected", (uid) => {
+        console.log("disconnected", uid)
+        if (calls[uid]) calls[uid].close()
+    })
+
+    function hasMediaDevises() {
+        return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
+    }
+
+    if (hasMediaDevises()) {
+        navigator.mediaDevices
+            .getUserMedia({
+                video: true,
+                audio: true,
+            })
+            .then((stream) => {
+                window.stream = stream
+                const myVideo = document.createElement("video")
+                myVideo.muted = true
+                addVideo(myVideo, stream)
+                peer.on("call", (call) => {
+                    console.log("recevied a call")
+                    call.answer(stream)
+                    let video = document.createElement("video")
+                    call.on("stream", (userVideoStream) => {
+                        addVideo(video, userVideoStream)
+                        recalculateLayout()
+                    })
                 })
+                // peer.on("error", (err) => console.log(err))
+                socket.on("user-connected", (user) => {
+                    console.log("new connection in room", user)
+                    setTimeout(() => {
+                        conntectToUser(user, stream)
+                    }, 1000)
+                })
+                recalculateLayout()
             })
-            // peer.on("error", (err) => console.log(err))
-            socket.on("user-connected", (user) => {
-                console.log("new connection in room", user)
-                setTimeout(() => {
-                    conntectToUser(user, stream)
-                }, 1000)
-            })
+    } else {
+        console.log("mediaDevice not suported in your device")
+    }
+
+    function conntectToUser(uid, stream) {
+        const call = peer.call(uid, stream)
+        console.log("calling", uid)
+        const video = document.createElement("video")
+        call.on("stream", (userVideoStream) => {
+            addVideo(video, userVideoStream)
             recalculateLayout()
         })
-} else {
-    console.log("mediaDevice not suported in your device")
-}
+        call.on("close", () => {
+            video.remove()
+            recalculateLayout()
+        })
+        calls[uid] = call
+        // call.on("error", (err) => console.log(err))
+    }
 
-function conntectToUser(uid, stream) {
-    const call = peer.call(uid, stream)
-    console.log("calling", uid)
-    const video = document.createElement("video")
-    call.on("stream", (userVideoStream) => {
-        addVideo(video, userVideoStream)
-        recalculateLayout()
-    })
-    call.on("close", () => {
-        video.remove()
-        recalculateLayout()
-    })
-    calls[uid] = call
-    // call.on("error", (err) => console.log(err))
-}
-
-function addVideo(video, stream, muted) {
-    let left = document.querySelector(".gallery")
-    let container = document.createElement("div")
-    container.classList.add("video_container")
-    video.srcObject = stream
-    video.addEventListener("loadedmetadata", () => {
-        video.play()
-    })
-    container.append(video)
-    left.append(container)
-    video_count++
+    function addVideo(video, stream, muted) {
+        let left = document.querySelector(".gallery")
+        let container = document.createElement("div")
+        container.classList.add("video_container")
+        video.srcObject = stream
+        video.addEventListener("loadedmetadata", () => {
+            video.play()
+        })
+        container.append(video)
+        left.append(container)
+        video_count++
+    }
 }
